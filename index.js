3dc6370a0e57bd75769b9cd9aa79ae9fadb755ae
McCray <tubach75@hotmail.com>
2021-06-26 13:35:46 -0400

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ class Formatter {
     return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
 
-  
+  static titleize(sentence){
+    let dontCap = ['a', 'the', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  let array = [];
+  let parts = sentence.split(' ');
+  for(let s of parts){
+    if(s == parts[0]){
+      array.push(this.capitalize(s));
+    } else {
+      if(dontCap.includes(s)){
+        array.push(s);
+      } else {
+        array.push(this.capitalize(s));
+      }
+    }
+
+}
+return array.join(' ');
+  }
 
 }
